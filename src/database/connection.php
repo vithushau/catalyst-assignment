63eb88234920b793491ed44c003db140276b39a6
unknown <vithushauthay@gmail.com>
2024-01-06 01:01:19 +1100

    Add initial project files

---
+++ src/database/connection.php
@@ -0,0 +1,18 @@
+<?php
+namespace Database;
+
+use mysqli;
+
+class Connection
+{
+    public static function make($host, $database, $username, $password)
+    {
+        $mysqli = new mysqli($host, $username, $password, $database);
+
+        if ($mysqli->connect_error) {
+            die("Connection failed: " . $mysqli->connect_error);
+        }
+
+        return $mysqli;
+    }
+}
